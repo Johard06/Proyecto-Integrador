2b6d457714eadea660f928d255f2aa57caddb367
johard <johards8d@gmail.com>
2024-08-09 04:44:07 -0400

    Proyect listo

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
+alert('Bienvenido a Digital Shop')
+
+const IniciarSesion = () => {
+
+    let correo = 'juan7@gmail.com';
+    let contraseña = '12345'
+
+    let correo1 = prompt ("Ingrese su correo");
+    let contraseña1 = prompt ("Ingrese su contraseña");
+
+    if (correo1 == correo && contraseña == contraseña) {
+        alert("Bienvenido Juan7");
+    }
+
+    if (correo1 == '' || isNaN(correo1) || correo1 == null && contraseña1 == '' || isNaN(contraseña1) || contraseña1 == null) {
+        alert('No tienes usuario? Registrate');
+    } else {
+        window.location = "../pages/registrate.html";
+    }
+}
 
